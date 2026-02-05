131619041a51f92df8a5f43f250a3baf770c2aa2
divyasumpagi-sketch <divyasumpagi@gmail.com>
2026-02-05 21:31:10 +0530

    Add files via upload
    Basic arithmetic calculator built using Python for performing fundamental math operations.

---
+++ Arithematic.py
@@ -0,0 +1,18 @@
+A = int (input("Enter the first integer"))
+B = int (input("Enter the second integer"))
+C = int (input("Enter the third integer"))
+
+add = A + B
+sub = A - B
+mult = A * C
+div = A / B
+mod = A % B
+
+
+print("addition : ",add)
+print("substraction : ",sub)
+print("multiplication : ",mult)
+print("division : ",div)
+print("modulo : ",mod)
+print("is A > B ? ", A>B)
+
